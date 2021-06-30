2784a66651a29b5b1e5218e875216f992c3efa68
Isha Slavin <isha_slavin@boriss-imac.myfiosgateway.com>
2021-06-30 09:03:01 -0400

    week 2 task 4 - made brief changes since last commit

--- ExampleCode/GLD_algorithm.py
+++ ExampleCode/GLD_algorithm.py
@@ -9,7 +9,8 @@ import numpy as np
 import pandas as pd
 import math
 import random
-from benchmarkfunctions import SparseQuadric
+from benchmarkfunctions import SparseQuadric, MaxK
+from matplotlib import pyplot as plt
 
 # let's write a function called GLD_algorithm().
 '''
@@ -30,29 +31,34 @@ def GLD_algorithm(defined_func, max_iter, x_0, D, R, r):
     K = math.ceil(K)
     print('K: ', K)
     list_of_xt = []
+    f_vals = []
     x_t = x_0
     # x = x_0
     print(x_t)
     list_of_xt.append(x_t)
+    f_vals.append(defined_func(x_t))
     for t in range(max_iter):
+        # print(t)
         # x_t = x_0[t]
         v_list = [x_t]
         for k in range(int(K)):
             r_k = 2 ** -k
             r_k = r_k * R
-            print('r_k: ', r_k)
+            # print('r_k: ', r_k)
             r_k_D = np.dot(r_k, D)
             random_dir = random.randint(0, n - 1)
             v_k = r_k_D[random_dir]
-            print('vk: ', v_k)
-            v_list.append(x_t + v_k)
+            # print('vk: ', v_k)
+            next_el = x_t + v_k
+            v_list.append(next_el)
         if len(v_list) == 0:
             list_of_xt.append(x_t)
+            f_vals.append(defined_func(x_t))
             continue
         while len(v_list) >= 2:
             new_instance_1 = Oracle(defined_func)
-            print('0:', v_list[0])
-            print('1:', v_list[1])
+            # print('0:', v_list[0])
+            # print('1:', v_list[1])
             first_comparison = new_instance_1(v_list[0], v_list[1])
             if first_comparison == +1:
                 # 0th elem is smaller.
@@ -67,10 +73,12 @@ def GLD_algorithm(defined_func, max_iter, x_0, D, R, r):
                 v_list.pop(rand_choice)
         list_length = len(v_list)
         if list_length == 1:
+            # print(t)
             x_t = v_list[0]
             list_of_xt.append(x_t)
+            f_vals.append(defined_func(x_t))
             continue
-    return x_t
+    return x_t, f_vals
 
 
 # sample invocation.
@@ -83,14 +91,23 @@ noise_amp = 0.001  # noise amplitude.
 # ---------
 # initialize objective functions.
 obj_func_1 = SparseQuadric(n_def, s_exact, noise_amp)
-max_iter = 10000
-x_0 = np.random.rand()
+# obj_func_2 = MaxK(n_def, s_exact, noise_amp)
+max_iterations = 10000
+x_0_ = np.random.rand(n_def)
 # Gaussian.
-D = np.random.randn(n_def)
-R = 10
-r = .01
-gld_1 = GLD_algorithm(obj_func_1, max_iter, x_0, D, R, r)
-print(gld_1)
+D_ = np.random.randn(n_def)
+R_ = 10
+r_ = .01
+gld_1 = GLD_algorithm(obj_func_1, max_iterations, x_0_, D_, R_, r_)
+# gld_1 = GLD_algorithm(obj_func_2, max_iterations, x_0_, D_, R_, r_)
+
+# print(gld_1)
+answer = gld_1[0]
+print('solution: ', answer)
+func_vals = gld_1[1]
+
+plt.plot(func_vals)
+plt.show()
 
 
 

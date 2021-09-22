2be4c39a71b884e02839d03e8bf826de603af996
Daniel Mckenzie <DanielMckenzie@users.noreply.github.com>
2021-09-22 16:00:55 -0700

    Fixed CMA

--- ExampleCode/CMA_2.py
+++ ExampleCode/CMA_2.py
@@ -29,21 +29,25 @@ class CMA(BaseOptimizer):
         self.lam = lam
         self.mu = mu
         self.sigma = sigma
-        self.p_sigma = 0
-        self.p_c = 0
+        self.p_sigma = np.zeros(self.dim)
+        self.p_c = np.zeros(self.dim)
         self.queries = 0
         self.function = function
         
         # For the following parameters we use the defaults
-        self.mu_eff = 1
-        self.c_mu = 0.5 
+        self.mu_eff = self.mu
+        self.c_mu = 1/len(x0)
         self.c_sigma = 0.5
+        self.c_c = 0.5
+        self.d_sigma = 0.25
+        self.h_sigma = 0.5
+        self.c1 = 1/len(x0)
 
     def step(self):
         # The following code samples self.lam vectors from a normal dist with
         # mean self.m and covariance self.sigma^2 * self.N
 
-        Yg = np.random.multivariate_normal(self.m, self.C, self.lam)
+        Yg = np.random.multivariate_normal(np.zeros(self.dim), self.C, self.lam)
         Xg = self.m + self.sigma*Yg
         # print(Xg.shape)
         # for i in range(self.mu):
@@ -52,22 +56,27 @@ class CMA(BaseOptimizer):
         # The next line sorts according to function values
         Sorted_Xg, num_queries = BubbleSort(Xg, self.oracle)
         Sorted_Yg = (Sorted_Xg - self.m)/self.sigma
-        print(Sorted_Yg)
+        print(Sorted_Yg.shape)
 
         # In the next line, we use weights w_i = 1/mu for all i
         y_w = np.sum(Sorted_Yg[0:self.mu, :], axis=0)/self.mu
-        print(y_w)
+        # print(y_w)
 
         # Update mean
         self.m += self.sigma*y_w
         
         # Update step size
-        # C_half_inverse = np.linalg.matrix_power(sqrtm(self.C + 0.01*np.eye(self.dim)), -1)
-        # self.p_sigma = (1-self.c_sigma)*self.p_sigma + np.sqrt(self.c_sigma*
-                       # (2-self.c_sigma)*self.mu_eff)*np.dot(C_half_inverse, y_w)
+        C_half_inverse = np.linalg.matrix_power(sqrtm(self.C), -1)
+        self.p_sigma = (1-self.c_sigma)*self.p_sigma + np.sqrt(self.c_sigma*
+                       (2-self.c_sigma)*self.mu_eff)*np.dot(C_half_inverse, y_w)
+        self.sigma *= np.exp((self.c_sigma/self.d_sigma)*(np.linalg.norm(self.p_sigma)/
+                              (np.sqrt(self.dim)*(1 - 1/(4*self.dim) + 1/(21*self.dim**2)))-1))
 
         # Update covariance matrix
-        # temp1 = (self.c_mu/self.mu)*np.dot(Sorted_Yg[0:self.mu,:].T, Sorted_Yg[0:self.mu,:])
-        # self.C = (1-self.c_mu/self.mu)*self.C + temp1
-        
-        return self.function(self.m)
+        self.p_c = (1 - self.c_c)*self.p_c + self.h_sigma*np.sqrt(self.c_c*(2-
+                   self.c_c)*self.mu_eff)*y_w
+        term2 = self.c1*np.outer(self.p_c, self.p_c)
+        term3 = (self.c_mu/self.mu)*np.dot(Sorted_Yg[0:self.mu,:].T, Sorted_Yg[0:self.mu,:])
+        self.C = (1 - self.c1 - self.c_mu/self.mu)*self.C + term2 + term3
+        tempval = self.function(self.m)
+        return tempval
--- ExampleCode/TestCMA.py
+++ ExampleCode/TestCMA.py
@@ -14,30 +14,33 @@ import matplotlib.pyplot as plt
 from benchmarkfunctions import SparseQuadratic, MaxK
 
 # Defining the function
-n_def = 20
-s_exact = 10
+n_def = 100
+s_exact = 20
 noise_amp = 0.0
 func = SparseQuadratic(n_def, s_exact, noise_amp)
 query_budget = int(1e5)
 m = 100
-x0 = np.random.randn(n_def)
+x0 = 100*np.random.randn(n_def)
 step_size = 0.2
 r = 0.1
-max_iters = int(2)
+max_iters = int(1000)
 lam = 10
-mu = 2
-sigma = 5
+mu = 5
+sigma = 0.5
 
 # Define the comparison oracle
 oracle = Oracle(func)
 
+all_func_vals = []
+
 Opt = CMA(oracle, query_budget, x0, lam, mu, sigma, function=func)
 
 for i in range(max_iters):
-    print(i)
+    # print(i)
     val = Opt.step()
     print(val)
+    all_func_vals.append(val)
     
-# plt.semilogy(Opt.f_vals)
-# plt.show()
+plt.semilogy(all_func_vals)
+plt.show()
     
